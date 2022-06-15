e112c2a0f23ccc1e907b5f22a15e8f0d893e44c5
krishnajhapate <ki54.jhapate@gmail.com>
2022-06-15 17:49:40 +0530

    view setup

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,65 +1,33 @@
-from rest_framework.generics import GenericAPIView
-from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
 from rest_framework.response import Response
-
-from backend.accounts.firebase import Firebase_validation
+from rest_framework.permissions import IsAuthenticated
+from firebase_admin import auth
+from django.contrib.auth import get_user_model
 # Create your views here.
 
-User = get_user_model()
 
-class AuthAPIView(GenericAPIView):
-    """
-    api for creating user from social logins
-    """
+class HelloView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        return Response({'message': 'Hello, World!'})
+
+
+class AuthenticatedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({'message': 'You Are Authenticated', 'user': request.user.username})
+
+
+class RegisterUser(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
-        auth_header = request.META.get('HTTP_AUTHORIZATION')
-
-        if auth_header:
-            id_token = auth_header.split(" ").pop()
-
-            validate = Firebase_validation(id_token)
-
-            if validate:
-                user = User.objects.filter(uid = validate["uid"]).first()
-
-                if user:
-                   
-                    data = {
-                       "id": user.id,
-                       "email": user.email,
-                       "name": user.name,
-                       "image": user.image,
-                       "type": "existing_user",
-                       "provider": validate['provider']
-                   }
-
-                    return Response({"data": data,"message": "Login Successful" })
-
-                else:
-
-                    user = User(email = validate['email'],
-                                     name = validate['name'],
-
-                                     uid = validate['uid'],
-
-                                     image = validate['image']
-
-                                     )
-
-                    user.save()
-
-                    data = {
-                       "id": user.id,
-                       "email": user.email,
-                       "name": user.name,
-                       "image": user.image,
-                       "type": "new_user",
-                       "provider": validate['provider']
-                   }
-
-                    return Response({"data": data,
-
-                                    "message": "User Created Successfully" })
-
-        else:
-               return Response({"message": "invalid token"})
+        User = get_user_model()
+        user = User.objects.get(username=request.user.username)
+        firebase_data = auth.get_user(user.username)
+        user.email = firebase_data.email
+        user.save()
+        return Response({'message': 'User Registered'})
